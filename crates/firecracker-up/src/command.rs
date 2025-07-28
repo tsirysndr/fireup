50fa704177beb81537e45ac28e4bed1645632a4d
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2025-07-28 10:48:49 +0300

    check for KVM support

---
+++ crates/firecracker-up/src/command.rs
@@ -0,0 +1,29 @@
+use anyhow::{anyhow, Context, Result};
+use std::process::{Command, Output, Stdio};
+
+pub fn run_command(command: &str, args: &[&str]) -> Result<Output> {
+    let mut cmd = Command::new(command);
+
+    let output = cmd
+        .args(args)
+        .stderr(Stdio::piped())
+        .output()
+        .with_context(|| format!("Failed to execute {}", command))?;
+
+    if !output.status.success() {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        return Err(anyhow!(
+            "Command {} failed: {} {} {} {}",
+            command,
+            stderr,
+            stdout,
+            args.iter()
+                .map(|s| s.to_string())
+                .collect::<Vec<_>>()
+                .join(" "),
+            output.status.code().unwrap_or(-1),
+        ));
+    }
+    Ok(output)
+}
--- crates/firecracker-up/src/main.rs
+++ crates/firecracker-up/src/main.rs
@@ -1,8 +1,15 @@
 use std::thread;
 
 use anyhow::Result;
+use owo_colors::OwoColorize;
+
+use crate::command::run_command;
+
+pub mod command;
 
 fn main() -> Result<()> {
+    check_kvm_support()?;
+
     firecracker_process::start()?;
 
     loop {
@@ -15,5 +22,22 @@ fn main() -> Result<()> {
 
     firecracker_prepare::prepare()?;
     firecracker_vm::setup()?;
+    Ok(())
+}
+
+pub fn check_kvm_support() -> Result<()> {
+    print!("[+] Checking for kvm support... ");
+
+    if !run_command("sh", &["-c", "lsmod | grep kvm"])
+        .map(|output| output.status.success())
+        .unwrap_or(false)
+    {
+        return Err(anyhow::anyhow!(
+            "KVM is not available. Please ensure KVM is enabled in your system."
+        ));
+    }
+
+    println!("{}", "[✓] OK".bright_green());
+
     Ok(())
 }
