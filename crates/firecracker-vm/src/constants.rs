fc2c030c5f5ca616b7fbadb978eb0891872c1711
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-08-15 10:11:34 +0300

    fix incorrect MASK_SHORT

--- crates/firecracker-vm/src/constants.rs
+++ crates/firecracker-vm/src/constants.rs
@@ -3,5 +3,5 @@ pub const BRIDGE_DEV: &str = "br0";
 pub const FIRECRACKER_SOCKET: &str = "/tmp/firecracker.sock";
 pub const BRIDGE_IP: &str = "172.16.0.1";
 pub const FC_MAC: &str = "06:00:AC:10:00:02";
-pub const MASK_SHORT: &str = "/30";
+pub const MASK_SHORT: &str = "/24";
 pub const GUEST_IP: &str = "vm0.firecracker.local";
